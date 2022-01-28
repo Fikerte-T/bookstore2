efb387ef551ff8bedd5c314d7dfc0d5d71138617
fikerte <tofikertesfaye@gmail.com>
2022-01-28 21:43:07 +0300

    Style AddBook component

--- src/components/books/AddBook.js
+++ src/components/books/AddBook.js
@@ -23,12 +23,16 @@ const AddBook = () => {
     setAuthor('');
   };
   return (
-    <div>
-      <h3>ADD NEW BOOK</h3>
+    <div className='add-book'>
+      <h3 className='add-book-title'>ADD NEW BOOK</h3>
       <form onSubmit={submitBookToStore}>
-        <input type="text" placeholder="title" name="title" onChange={(e) => setTitle(e.target.value)} value={title} />
-        <input type="text" placeholder="author" name="author" onChange={(e) => setAuthor(e.target.value)} value={author} />
-        <button type="submit">Add book</button>
+        <input className='inputs' type="text" placeholder="title" name="title" onChange={(e) => setTitle(e.target.value)} value={title} />
+        {/* <input type="text" placeholder="author" name="author" onChange={(e) => setAuthor(e.target.value)} value={author} /> */}
+				<select required className="inputs category" name="Category" onChange={(e) => setCategory(e.target.value)}>
+          <option value="" disabled selected>Category</option>
+          <option value="Fiction">Fiction</option>
+        </select>
+        <button className='btn' type="submit">Add book</button>
       </form>
     </div>
   );
