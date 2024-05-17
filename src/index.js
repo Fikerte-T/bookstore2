ba9eb74db665710e949f89c4220a7aea5e67c9b1
fikerte <tofikertesfaye@gmail.com>
2024-05-17 18:13:23 +0300

    Update React version to support createROot

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/configureStore';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
@@ -14,5 +17,4 @@ ReactDOM.render(
       </Router>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
